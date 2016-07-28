b88b18d07a456358d6d5814e5b6f73febfdb6004
DenisThierry <Dt019@hdm-stuttgart.de>
2016-07-28 21:08:09 +0200

    add ressources

--- src/main/java/OpenNLPCategorizer.java
+++ src/main/java/OpenNLPCategorizer.java
@@ -13,27 +13,30 @@ import opennlp.tools.util.TrainingParameters;
 
 public class OpenNLPCategorizer {
     DoccatModel model = null;
+    String inputText = null;
 
     public static void main(String[] args) {
         OpenNLPCategorizer twitterCategorizer = new OpenNLPCategorizer();
         twitterCategorizer.trainModel();
         twitterCategorizer.catModel();
-        String[] inputTexts = {"I love you", "I am sick", "I am tired", "i really do not like harry potter", "damn, i hate this movie"};
+        twitterCategorizer.catInput();
+
+/*        String[] inputTexts = {"I hate tea", "I am sick of this tea", "I am tired of drinking tea", "i really do not like tea", "tea isn't exactly my favorite beverage"};
         for (String inputText : inputTexts) {
             twitterCategorizer.catTest(inputText);
-        }
+        }*/
     }
 
     public void trainModel() {
-        File file = new File ("/Users/chillingwithasmile/Downloads/train2.tsv");
+        File file = new File ("ressources/train2.tsv");
         MarkableFileInputStreamFactory dataIn = null;
         try {
             dataIn = new MarkableFileInputStreamFactory(file);
             ObjectStream lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
             ObjectStream sampleStream = new DocumentSampleStream(lineStream);
 
-            //Finden der Problem line
-           /* String sampleString;
+            //Finden der Problem line von Hr Eckert
+          /* String sampleString;
             int ln = 0;
             while ((sampleString = (String) lineStream.read())!=null){
 
@@ -44,6 +47,7 @@ public class OpenNLPCategorizer {
                     throw new IOException("Empty lines, or lines with only a category string are not allowed! AND THE TOKEN IS: " + tokens[0] + " AT THE FUCKING LINE " + ln);
                 }
             }*/
+
                 TrainingParameters mlParams = new TrainingParameters();
                 mlParams.put("Algorithm", "MAXENT");
                 mlParams.put("TrainerType", "Event");
@@ -74,37 +78,53 @@ public class OpenNLPCategorizer {
         }
     }
 
-    public void catTest(String inputText) {
+    public void catInput() {
+        try{
+            FileInputStream filestream = new FileInputStream("ressources/trumptweetsclean.txt");
+            DataInputStream in = new DataInputStream(filestream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            while ((inputText = br.readLine()) != null)   {
+                    catOutput(inputText);
+            }
+            in.close();
+
+        }catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void catOutput(String inputText) {
+
         InputStream modelIn = null;
         try {
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ressources/testbewertet.csv", true)));
             modelIn = new FileInputStream("ressources/twitterSen.bin");
             model = new DoccatModel(modelIn);
             DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
             double[] outcomes = myCategorizer.categorize(inputText);
 
-          /*  for (int i = 0; i<myCategorizer.getNumberOfCategories(); i++) {
-                String categoryOne = myCategorizer.getCategory(i);*/
-
-
             String category = myCategorizer.getBestCategory(outcomes);
             String outcome = myCategorizer.getAllResults(outcomes);
 
-
             if (category.equalsIgnoreCase("0")) {
-                System.out.println("Category: 0 - The tweet is negative with a possibility of " + outcome);
+                pw.append(inputText + ";0\n");
+                System.out.println(inputText + ": Category: 0 - The tweet is negative with a possibility of: " + outcome);
             } else if (category.equalsIgnoreCase("1")) {
-                System.out.println("Category: 1 - The tweet is somewhat negative with a possibility of " + outcome);
+                pw.append(inputText + ";1\n");
+                System.out.println(inputText + ": Category: 1 - The tweet is somewhat negative with a possibility of: " + outcome);
             } else if (category.equalsIgnoreCase("2")) {
-                System.out.println("Category: 2 - The tweet is neutral with a possibility of " + outcome);
-            } else if (category.equals("3")) {
-                System.out.println("Category: 3 - The tweet is somewhat positive with a possibility of " + outcome);
-            } else if (category.equals("4")) {
-                System.out.println("Category: 4 - The tweet is positive with a possibility of " + outcome);
+                pw.append(inputText + ";2\n");
+                System.out.println(inputText + ": Category: 2 - The tweet is neutral with a possibility of: " + outcome);
+            } else if (category.equalsIgnoreCase("3")) {
+                pw.append(inputText + ";3\n");
+                System.out.println(inputText + ": Category: 3 - The tweet is somewhat positive with a possibility of: " + outcome);
+            } else if (category.equalsIgnoreCase("4")) {
+                pw.append(inputText + ";4\n");
+                System.out.println(inputText + ": Category: 4 - The tweet is positive with a possibility of: " + outcome);
             }
 
-                //System.out.println(categoryOne + " - " + outcomes[i]);
-            //} //for schleifenende
-
+            pw.close();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
